8c015efcf338509f698c0e1db6a894d90cbf2358
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2023-04-30 17:14:47 +0200

    added image preoptimization

--- optimize_images.js
+++ optimize_images.js
@@ -1,30 +1,30 @@
-const sharp = require(`sharp`)
-const glob = require(`glob`)
-const fs = require(`fs-extra`)
+const sharp = require(`sharp`);
+const glob = require(`glob`);
+const fs = require(`fs-extra`);
 
-const matches = glob.sync(`blog/**/*.{png,jpg,jpeg}`)
-const MAX_WIDTH = 1800
-const QUALITY = 70
+const matches = glob.sync(`blog/**/*.{png,jpg,jpeg}`);
+const MAX_WIDTH = 1800;
+const QUALITY = 70;
 
 Promise.all(
-  matches.map(async match => {
-    const stream = sharp(match)
-    const info = await stream.metadata()
+  matches.map(async (match) => {
+    const stream = sharp(match);
+    const info = await stream.metadata();
 
     if (info.width < MAX_WIDTH) {
-      return
+      return;
     }
 
     const optimizedName = match.replace(
       /(\..+)$/,
       (match, ext) => `-optimized${ext}`
-    )
+    );
 
     await stream
       .resize(MAX_WIDTH)
       .jpeg({ quality: QUALITY })
-      .toFile(optimizedName)
+      .toFile(optimizedName);
 
-    return fs.rename(optimizedName, match)
+    return fs.rename(optimizedName, match);
   })
-)
+);
