01f0ad543b27804fd86b7237b600fd73a86324f1
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2023-04-30 13:02:12 +0200

    added eslint to precommit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,28 +1,18 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
-    },
-    "extends": [
-        "eslint:recommended",
-        "plugin:react/recommended",
-        "react-app",
-    ],
-    "overrides": [
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
-    },
-    "plugins": [
-        "react"
-    ],
-    "rules": {
-        "react/prop-types": "off",
-        "react/no-unescaped-entities": "off"
-    },
-    "ignorePatterns": [
-        "public/*",
-        ".eslintrc.js"
-    ]
-}
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended", "react-app"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/prop-types": "off",
+    "react/no-unescaped-entities": "off",
+  },
+  ignorePatterns: ["public/*", ".eslintrc.js"],
+};
