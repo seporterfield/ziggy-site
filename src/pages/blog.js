173ec7e0b77d79a9a1e7970821c9c41506424dae
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2023-04-30 13:57:58 +0200

    using mdx in gql data layer for blogs

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,15 +6,13 @@ import Seo from "../components/seo";
 const BlogPage = ({ data }) => {
   return (
     <Layout pageTitle="My Blog Posts">
-      {
-        data.allMdx.nodes.map((node) => (
-          <article key={node.id}>
-            <h2>{node.frontmatter.title}</h2>
-            <p>Posted: {node.frontmatter.date}</p>
-            <p>{node.excerpt}</p>
-          </article>
-        ))
-      }
+      {data.allMdx.nodes.map((node) => (
+        <article key={node.id}>
+          <h2>{node.frontmatter.title}</h2>
+          <p>Posted: {node.frontmatter.date}</p>
+          <p>{node.excerpt}</p>
+        </article>
+      ))}
     </Layout>
   );
 };
