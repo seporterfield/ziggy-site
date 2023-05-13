bea509777919ae952e06890f2eb1285542903c49
Siegfried Porterfield <siegfriedporterfield@gmail.com>
2023-05-13 17:39:22 +0200

    made blog landing page, abstracted out post links

--- src/components/postlink.js
+++ src/components/postlink.js
@@ -5,9 +5,7 @@ const PostLink = ({ slug, title, date, excerpt }) => {
   return (
     <div>
       <h2>
-        <Link to={`/posts/${slug}`}>
-          {title}
-        </Link>
+        <Link to={`/posts/${slug}`}>{title}</Link>
       </h2>
       <p>Posted: {date}</p>
       <p>{excerpt}</p>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,13 +1,9 @@
-import * as React from "react"
+import * as React from "react";
 import Layout from "../../components/layout";
 import Seo from "../../components/seo";
 
 const ProjectPage = () => {
-  return (
-    <Layout pageTitle="My Projects">
-      Coming soon!
-    </Layout>
-  );
+  return <Layout pageTitle="My Projects">Coming soon!</Layout>;
 };
 
 export const Head = () => <Seo title="My Projects" />;
